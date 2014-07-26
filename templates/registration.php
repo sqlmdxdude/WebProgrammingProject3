2bfc7e433a76af5de30a119fbaaf595ae183f48b
Simmigon Flagg <simmigon.flagg@gmail.com>
2014-07-25 22:42:30 -0400

    Moved the registration

---
+++ templates/registration.php
@@ -0,0 +1,26 @@
+
+<?php
+	$title ="Registration Page";
+    include_once "../templates/banner.php"; 
+	include_once "../templates/footer.php";
+	include_once "../templates/menunavigation.php"; 
+	include_once "../templates/footer.php";
+?>
+<!DOCTYPE html> 
+<html lang = "en">
+<!-- http://localhost/WebProgrammingProject3/templates/registration.php* -->
+	<head >
+		<title><?php echo $title ?></title>
+		<meta charset="UTF-8">
+		<script type="text/javascript"></script>
+		<link rel="stylesheet" text="text/css" href="../css/sitetemplate.css">
+	</head>
+	<body>
+	<div id="banner"><?php echo $banner; ?></div> 
+	 <?php echo $menunavi; ?>
+		<div id="content">
+	
+		</div>	
+		<?php echo $footer; ?>
+	</body>
+</html>
