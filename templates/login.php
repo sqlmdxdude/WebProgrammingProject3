28003acbad1062f8f5cadce96ec70d92439908d9
Simmigon Flagg <simmigon.flagg@gmail.com>
2014-07-28 15:12:52 -0400

    This is the lastest

---
+++ templates/login.php
@@ -0,0 +1,22 @@
+<!-- Http://localhost/WebProgrammingProject3/templatesindex.php -->
+
+<?php
+$login =	'<form action=" Http://localhost/LoginSystem/loginTest.php" method="POST">
+			<div>
+				<table>
+					<tr>
+						<td><label>Enter Your Name:<label></td>
+						<td><input name="username" type="text" size="19" /></td>
+					</tr>
+					<tr>
+						<td><label>Enter Your Password:<label></td>
+						<td><input name="password" type="password" size="19" /></td>
+					</tr>
+					<tr>
+						<td><!-- Left blank on purpose--></td>
+						<td><input type="submit" value="Login" /><input type="reset" /></td>
+					</tr>
+				</table>
+			</div>
+		</form>' 
+?>
